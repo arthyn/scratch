2f34455645bab156488177716ece78452b22e97c
Hunter Miller <hunter@hmiller.dev>
2023-06-08 00:03:13 -0500

    removing unused ui folder

--- ui/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv extends Readonly<Record<string, string | boolean | undefined>> {
-  /* Add custom env properties here */
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
